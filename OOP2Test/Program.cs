2d96c230adbd135d9811766a187ef9cdc90d1d14
Mads Bock <bockchristensen@gmail.com>
2021-01-03 15:52:07 +0100

    initialized project

---
+++ OOP2Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OOP2;
+
+namespace OOP2Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test that a person can be created.
+            Person person = new Person("Mads", 30);
+
+            Console.WriteLine("Tjek at det er det korrekte navn og alder der står herunder:");
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
